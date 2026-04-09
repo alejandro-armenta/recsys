880bedbbd0d3fe93f991971cbee4503bef1351c9
Alejandro <armentaayalaalejandro@gmail.com>
2026-04-08 22:27:51 -0600

    added data

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-/home/alejandro-armenta/anaconda3/envs/rec/bin/python /home/alejandro-armenta/REC/randomrec.py --input_file=fashion-cat.json --output_html=output.html
+#/home/alejandro-armenta/anaconda3/envs/rec/bin/python /home/alejandro-armenta/REC/randomrec.py --input_file=fashion-cat.json --output_html=output.html
+#/home/alejandro-armenta/anaconda3/envs/rec/bin/python /home/alejandro-armenta/REC/ip.py
+
+/home/alejandro-armenta/anaconda3/envs/rec2/bin/python /home/alejandro-armenta/REC/ip.py
---
+++ ip.py
@@ -0,0 +1,14 @@
+
+from typing import Sequence, Tuple
+
+import tensorflow as tf
+
+print(tf.config.list_physical_devices('GPU'))
+
+def create_dataset(triplet: Sequence[Tuple[str,str,str]]):
+
+    tf
+    pass
+
+
+create_dataset(('ale','jorge','maria'))
